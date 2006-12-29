ac8d67999a0a83bb6215ad0439eff8cd09ac75ed
nekohayo <>
2006-12-29 20:32:10 +0000

    There was a small glitch in the new import_export module where it tried to import from specto, but it needs to actually import from spectlib. Fixed.

--- spectlib/import_export.py
+++ spectlib/import_export.py
@@ -21,7 +21,7 @@
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
-from specto.i18n import _
+from spectlib.i18n import _
 import os
 from spectlib.iniparser import ini_namespace
 
