9d13d41776cc3d51660b9a0a2abe7a8f5eff5332
chalserogers <>
2007-01-17 03:26:15 +0000

    Factor out callback-related functionality of *Listeners

--- spectlib/net/connectionmanager.py
+++ spectlib/net/connectionmanager.py
@@ -1,7 +1,23 @@
 import dbus
 import urllib2
 
-class NMListener(object):
+class CallbackRunner(object):
+    def __init__(self):
+        self.callbacks = {}
+    
+    def add_callback(self, callback, *args, **kwargs):
+        self.callbacks[callback] = (args, kwargs)
+
+    def _run_callbacks(self):
+        # We can't delete items from a dict we're iterating over
+        # so we must make a copy first
+        cb = dict(self.callbacks)
+        for fn, (args, kwargs) in cb.iteritems():
+            fn(*args, **kwargs)
+            del self.callbacks[fn]
+    
+
+class NMListener(CallbackRunner):
     statusTable = {0: u'Unknown',
                    1: u'Asleep',
                    2: u'Connecting',
@@ -9,12 +25,12 @@ class NMListener(object):
                    4: u'Disconnected' }
     
     def __init__(self, bus):
+        super(NMListener, self).__init__()
         self.nmProxy = bus.get_object('org.freedesktop.NetworkManager',
                                       '/org/freedesktop/NetworkManager')
         self.nmIface = dbus.Interface(self.nmProxy,
                                       'org.freedesktop.NetworkManager')
-        self.callbacks = {}
-        
+    
     def connected(self):
         return self.nmIface.state() == 3
 
@@ -27,18 +43,7 @@ class NMListener(object):
             return False
         return True
 
-    def add_callback(self, callback, *args, **kwargs):
-        self.callbacks[callback] = (args, kwargs)
-
-    def _run_callbacks(self):
-        # We can't delete items from a dict we're iterating over
-        # so we must make a copy first
-        cb = dict(self.callbacks)
-        for fn, (args, kwargs) in cb.iteritems():
-            fn(*args, **kwargs)
-            del self.callbacks[fn]
-
-class FallbackListener(object) :
+class FallbackListener(CallbackRunner) :
     def connected(self):
         try:
             # try if google can be reached, i.e. connection to internet is up
--- spectlib/net/test/test_connectionmanager.py
+++ spectlib/net/test/test_connectionmanager.py
@@ -1,5 +1,6 @@
 import unittest
-from spectlib.net.connectionmanager import NMListener, FallbackListener
+from spectlib.net.connectionmanager import (NMListener, FallbackListener,
+                                            CallbackRunner)
 import dbus
 import urllib2
 
@@ -29,6 +30,81 @@ class LogingCallback(object):
     def __call__(self, *args, **kwargs):
         self.log.append((args, kwargs))
 
+class TestCallbackRunner(unittest.TestCase):
+    def setUp(self):
+        self.cbRunner = CallbackRunner()
+        
+    def test_addingCallback(self):
+        """
+        Add a callback and trigger it
+        """
+        callback = LogingCallback()
+        self.cbRunner.add_callback(callback)
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(tuple() , { })], callback.log)
+
+    def test_callbackArgs(self):
+        """
+        Add a callback, with some args, and trigger it
+        """
+        callback = LogingCallback()
+        self.cbRunner.add_callback(callback, 'foo', 'bar')
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(('foo', 'bar') , { })], callback.log)
+
+    def test_callbackKWArgs(self):
+        """
+        Add a callback, with some KWArgs, and trigger it
+        """
+        callback = LogingCallback()
+        self.cbRunner.add_callback(callback, foo='bar')
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(tuple() , {'foo' : 'bar'})], callback.log)
+        
+    def test_callbackArgsAndKWargs(self):
+        """
+        Add a callback, with both Args and KWArgs, and trigger it.
+        """
+        callback = LogingCallback()
+        self.cbRunner.add_callback(callback, 'empathy', foo='bar')
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(('empathy', ) , {'foo' : 'bar'})], callback.log)
+        
+    def test_multipleCallbacks(self):
+        """
+        Add two callbacks.  Each should be run with its specified arguments
+        """
+        cb1 = LogingCallback()
+        cb2 = LogingCallback()
+        self.cbRunner.add_callback(cb1, 'cb1 called')
+        self.cbRunner.add_callback(cb2, 'cb2 called')
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(('cb1 called',), {})], cb1.log)
+        self.assertEqual([(('cb2 called',), {})], cb2.log)
+
+    def test_registerCallbackMultipleTimes(self):
+        """
+        Registering a callback twice should update the args and kwargs, but
+        NOT cause it to be called twice
+        """
+        callback = LogingCallback()
+        self.cbRunner.add_callback(callback, 'foo')
+        self.cbRunner.add_callback(callback, 'bar')
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(('bar', ) , {})], callback.log)
+
+    def test_callbackOnlyRunOnce(self):
+        """
+        Callbacks should be removed after they are run.  Each call to
+        add_callback should trigger at most one callback
+        """
+        callback = LogingCallback()
+        self.cbRunner.add_callback(callback, 'foo')
+        self.cbRunner._run_callbacks()
+        self.cbRunner._run_callbacks()
+        self.assertEqual([(('foo', ) , {})], callback.log)
+
+
 class TestNMConnectionListener(unittest.TestCase) :
     def setUp(self):
         bus = dbus.SystemBus()
@@ -51,75 +127,7 @@ class TestNMConnectionListener(unittest.TestCase) :
     def test_hasNetworkManager(self):
         self.assertTrue(self.nmListener.has_networkmanager())
 
-    def test_addingCallback(self):
-        """
-        Add a callback and trigger it
-        """
-        callback = LogingCallback()
-        self.nmListener.add_callback(callback)
-        self.nmListener._run_callbacks()
-        self.assertEqual([(tuple() , { })], callback.log)
 
-    def test_callbackArgs(self):
-        """
-        Add a callback, with some args, and trigger it
-        """
-        callback = LogingCallback()
-        self.nmListener.add_callback(callback, 'foo', 'bar')
-        self.nmListener._run_callbacks()
-        self.assertEqual([(('foo', 'bar') , { })], callback.log)
-
-    def test_callbackKWArgs(self):
-        """
-        Add a callback, with some KWArgs, and trigger it
-        """
-        callback = LogingCallback()
-        self.nmListener.add_callback(callback, foo='bar')
-        self.nmListener._run_callbacks()
-        self.assertEqual([(tuple() , {'foo' : 'bar'})], callback.log)
-        
-    def test_callbackArgsAndKWargs(self):
-        """
-        Add a callback, with both Args and KWArgs, and trigger it.
-        """
-        callback = LogingCallback()
-        self.nmListener.add_callback(callback, 'empathy', foo='bar')
-        self.nmListener._run_callbacks()
-        self.assertEqual([(('empathy', ) , {'foo' : 'bar'})], callback.log)
-        
-    def test_multipleCallbacks(self):
-        """
-        Add two callbacks.  Each should be run with its specified arguments
-        """
-        cb1 = LogingCallback()
-        cb2 = LogingCallback()
-        self.nmListener.add_callback(cb1, 'cb1 called')
-        self.nmListener.add_callback(cb2, 'cb2 called')
-        self.nmListener._run_callbacks()
-        self.assertEqual([(('cb1 called',), {})], cb1.log)
-        self.assertEqual([(('cb2 called',), {})], cb2.log)
-
-    def test_registerCallbackMultipleTimes(self):
-        """
-        Registering a callback twice should update the args and kwargs, but
-        NOT cause it to be called twice
-        """
-        callback = LogingCallback()
-        self.nmListener.add_callback(callback, 'foo')
-        self.nmListener.add_callback(callback, 'bar')
-        self.nmListener._run_callbacks()
-        self.assertEqual([(('bar', ) , {})], callback.log)
-
-    def test_callbackOnlyRunOnce(self):
-        """
-        Callbacks should be removed after they are run.  Each call to
-        add_callback should trigger at most one callback
-        """
-        callback = LogingCallback()
-        self.nmListener.add_callback(callback, 'foo')
-        self.nmListener._run_callbacks()
-        self.nmListener._run_callbacks()
-        self.assertEqual([(('foo', ) , {})], callback.log)
 
 def mockFailingUrlOpen(url) :
     raise IOError('foo')
@@ -146,7 +154,8 @@ class TestFallbackConnectionListener(unittest.TestCase) :
     def test_disconnected(self):
         urllib2.urlopen = mockFailingUrlOpen
         self.assertFalse(self.fbListener.connected())
-        
+
+
         
 if __name__ == '__main__':
     unittest.main()
