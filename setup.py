4bb4abdb8434957fcfafe57e3fc437a1719af253
Francesco Marella <fmarl@paranoici.org>
2011-07-29 12:01:35 +0200

    Rework setup.py script

--- setup.py
+++ setup.py
@@ -14,6 +14,15 @@ INSTALLED_FILES = "installed_files"
 
 from spectlib import __pkg_version__ as version
 
+def give_files(dir, *extension):
+    files=[]
+    all_files=os.listdir(dir)
+    for file in all_files:
+        ext=(os.path.splitext(file))[1]
+        if ext in extension:
+            files.append(dir + file)
+    return files
+
 class install (_install):
 
     def run (self):
@@ -80,57 +89,45 @@ if len (sys.argv) < 2 or sys.argv[1] not in ops:
     print "Please specify operation : %s" % " | ".join (ops)
     raise SystemExit
 
-prefix = None
-if len (sys.argv) > 2:
-    i = 0
-    for o in sys.argv:
-        if o.startswith ("--prefix"):
-            if o == "--prefix":
-                if len (sys.argv) >= i:
-                    prefix = sys.argv[i + 1]
-                sys.argv.remove (prefix)
-            elif o.startswith ("--prefix=") and len (o[9:]):
-                prefix = o[9:]
-            sys.argv.remove (o)
-            break
-        i += 1
-if not prefix and "PREFIX" in os.environ:
-    prefix = os.environ["PREFIX"]
-if not prefix or not len (prefix):
-    prefix = "/usr/local"
 
-if sys.argv[1] in ("install", "uninstall") and len (prefix):
-    sys.argv += ["--prefix", prefix]
+if sys.argv[1] in ("build", "install"):
+    prefix = None
+    if len (sys.argv) > 2:
+        i = 0
+        for o in sys.argv:
+            if o.startswith ("--prefix"):
+                if o == "--prefix":
+                    if len (sys.argv) >= i:
+                        prefix = sys.argv[i + 1]
+                    sys.argv.remove (prefix)
+                elif o.startswith ("--prefix=") and len (o[9:]):
+                    prefix = o[9:]
+                sys.argv.remove (o)
+                break
+            i += 1
+    if not prefix and "PREFIX" in os.environ:
+        prefix = os.environ["PREFIX"]
+    if not prefix or not len (prefix):
+        prefix = "/usr/local"
 
-with open(os.path.join ("spectlib/constants.py.in"), "rt") as file_in:
-    data = file_in.read ()
-    data = data.replace ("@prefix@", prefix)
-    data = data.replace ("@version@", version)
-    with open (os.path.join ("spectlib/constants.py"), "wt") as file_out:
-        file_out.write (data)
+    if sys.argv[1] in ("install", "uninstall") and len (prefix):
+#    if len (prefix):
+        sys.argv += ["--prefix", prefix]
 
-with open(os.path.join("data/indicator/specto.in"), "rt") as file_in:
-    data = file_in.read()
-    data = data.replace("@prefix@", prefix)
-    with open (os.path.join ("data/indicator/specto"), "wt") as file_out:
-        file_out.write(data)
+    if sys.argv[1] == "build":
+        with open(os.path.join("data/indicator/specto.in"), "rt") as file_in:
+            data = file_in.read()
+            data = data.replace("@prefix@", prefix)
+            with open (os.path.join ("data/indicator/specto"), "wt") as file_out:
+                file_out.write(data)
 
-cmd = "intltool-merge -d -u po/ specto.desktop.in specto.desktop".split(" ")
-try:
-    proc = subprocess.Popen(cmd)
-    proc.wait()
-except:
-    print "Error: intltool-merge not found, please install the intltool package."
-    raise SystemExit
-
-def give_files(dir, *extension):
-    files=[]
-    all_files=os.listdir(dir)
-    for file in all_files:
-        ext=(os.path.splitext(file))[1]
-        if ext in extension:
-            files.append(dir + file)
-    return files
+        cmd = "intltool-merge -d -u po/ data/desktop/specto.desktop.in specto.desktop".split(" ")
+        try:
+            proc = subprocess.Popen(cmd)
+            proc.wait()
+        except:
+            print "Error: intltool-merge not found, please install the intltool package."
+            raise SystemExit
 
 custom_images = []
 
@@ -144,47 +141,67 @@ data_files = [
 global_icon_path = "share/icons/hicolor"
 local_icon_path = "share/specto/icons/hicolor/"
 
-for dir, subdirs, files in os.walk("data/icons/"):
-    if dir == "data/icons/":
-        for file in files:
-            custom_images.append(dir + file)
-    else:
-        images = []
-        global_images = []
+if sys.argv[1] in ("build", "install"):
+    for dir, subdirs, files in os.walk("data/icons/"):
+        if dir == "data/icons/":
+            for file in files:
+                custom_images.append(dir + file)
+        else:
+            images = []
+            global_images = []
 
-        for file in files:
-            if file.find(".svg") or file.find(".png"):
-                file_path = "%s/%s" % (dir, file)
-                # global image
-                if file[:-4] == "specto":
-                    global_images.append(file_path)
-                # local image
-                else:
-                    images.append(file_path)
-        # local
-        if len(images) > 0:
-            data_files.append((local_icon_path + dir[10:], images))
-        # global
-        if len(global_images) > 0:
-            data_files.append((global_icon_path + dir[10:], global_images))
+            for file in files:
+                if file.find(".svg") or file.find(".png"):
+                    file_path = "%s/%s" % (dir, file)
+                    # global image
+                    if file[:-4] == "specto":
+                        global_images.append(file_path)
+                    # local image
+                    else:
+                        images.append(file_path)
+            # local
+            if len(images) > 0:
+                data_files.append((local_icon_path + dir[10:], images))
+            # global
+            if len(global_images) > 0:
+                data_files.append((global_icon_path + dir[10:], global_images))
 
-data_files.append(("share/specto/icons/", custom_images))
+    data_files.append(("share/specto/icons/", custom_images))
 
-podir = os.path.join (os.path.realpath ("."), "po")
-if os.path.isdir (podir):
-    buildcmd = "msgfmt -o build/locale/%s/specto.mo po/%s.po"
-    mopath = "build/locale/%s/specto.mo"
-    destpath = "share/locale/%s/LC_MESSAGES"
-    for name in os.listdir (podir):
-        if name[-2:] == "po":
-            name = name[:-3]
-            if sys.argv[1] == "build" \
-               or (sys.argv[1] == "install" and \
-                   not os.path.exists (mopath % name)):
-                if not os.path.isdir ("build/locale/" + name):
-                    os.makedirs ("build/locale/" + name)
-                os.system (buildcmd % (name, name))
-            data_files.append ((destpath % name, [mopath % name]))
+    podir = os.path.join (os.path.realpath ("."), "po")
+    if os.path.isdir (podir):
+        buildcmd = "msgfmt -o build/locale/%s/specto.mo po/%s.po"
+        mopath = "build/locale/%s/specto.mo"
+        destpath = "share/locale/%s/LC_MESSAGES"
+        for name in os.listdir (podir):
+            if name[-2:] == "po":
+                name = name[:-3]
+                if sys.argv[1] == "build" \
+                   or (sys.argv[1] == "install" and \
+                       not os.path.exists (mopath % name)):
+                    if not os.path.isdir ("build/locale/" + name):
+                        os.makedirs ("build/locale/" + name)
+                    os.system (buildcmd % (name, name))
+                data_files.append ((destpath % name, [mopath % name]))
+
+    if sys.argv[1] == "install":
+        gtk_update_icon_cache = "gtk-update-icon-cache -f -t \
+    %s/share/icons/hicolor" % prefix
+        root_specified = len (filter (lambda s: s.startswith ("--root"),
+                                      sys.argv)) > 0
+        if not root_specified:
+            print "Updating Gtk icon cache."
+            os.system (gtk_update_icon_cache)
+        else:
+            print '''*** Icon cache not updated. After install, run this:
+    ***     %s''' % gtk_update_icon_cache
+
+if sys.argv[1] == "clean":
+    try:
+        os.remove ("data/indicator/specto")
+        os.remove ("specto.desktop")
+    except:
+        pass
 
 
 setup(name = "specto",
@@ -200,19 +217,3 @@ setup(name = "specto",
                 "install" : install,
                 "install_data" : install_data}
     )
-
-os.remove ("spectlib/constants.py")
-os.remove ("data/indicator/specto")
-os.remove ("specto.desktop")
-
-if sys.argv[1] == "install":
-    gtk_update_icon_cache = '''gtk-update-icon-cache -f -t \
-%s/share/icons/hicolor''' % prefix
-    root_specified = len (filter (lambda s: s.startswith ("--root"),
-                                  sys.argv)) > 0
-    if not root_specified:
-        print "Updating Gtk icon cache."
-        os.system (gtk_update_icon_cache)
-    else:
-        print '''*** Icon cache not updated. After install, run this:
-***     %s''' % gtk_update_icon_cache
